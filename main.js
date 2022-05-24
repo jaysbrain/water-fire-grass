d9da933c76ca416b51a7fbd298f0867ee01a1ffb
Jay Ybarra <jybarra77@student.alamo.edu>
2022-05-23 22:25:00 -0500

    Create game, playRound, and playerChoice functions

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 const choices = ["water", "fire", "grass"];
 
+function game() {
+  playRound();
+}
+
+function playRound() {
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
+}
+
 function computerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playerChoice() {
+  let input = prompt("Type Water, Fire, or Grass");
+
+  while (input == null) {
+    input = prompt("Type Water, Fire, or Grass");
+  }
+
+  input = input.toLowerCase();
 }
